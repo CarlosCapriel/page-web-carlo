685b40f8f8771dc3678242ccb5e7a47340582ccb
Carlos-Capriel <ccapriel@auronix.com>
2021-11-13 17:10:20 -0600

    home, nav e index echo

---
+++ public/nav.php
@@ -0,0 +1,7 @@
+<nav class="bg-gray-700 text-blue-100">
+    <ul class="flex flex-row">
+        <li class="ml-8 pr-8 hover:bg-gray-800 p-4"><a href="">Inicio</a></li>
+        <li class="pr-8 hover:bg-gray-800 p-4"><a href="">Login</a></li>
+        <li class="pr-8 hover:bg-gray-800 p-4"><a href="">Registrarse</a></li>
+    </ul>
+</nav>
