358e785e57510ab3fe3ced726b86f23f4cf8ba90
Carlos-Capriel <ccapriel@auronix.com>
2021-12-02 21:44:15 -0600

    agregando todas las tecnologias utilizadas

--- public/views/tecnologias.php
+++ public/views/tecnologias.php
@@ -5,7 +5,19 @@
     } else {
         ?>
 <div class="py-24 flex items-center justify-center">
-    <div class="bg-blue-200 rounded-lg shadow-2x1 w-1/5">
+    <div class="bg-blue-200 rounded-lg shadow-2x1 w-1/5 m-5">   
+        <img src="public/images/html_logo.png" alt="" 
+                class="rounded-t-lg h-48 w-full">
+        <h2 class="text-x1 font-extrabold px-8">HTML</h2>
+        <div class="pl-8 pr-8 pb-8">
+            <p class="truncate">HTML (Lenguaje de Marcas de Hipertexto, del inglés HyperText Markup Language) 
+                es el componente más básico de la Web</p>
+            <button class="bg-blue-500 text-blue-100 rounded-lg py-2 px-4 mt-5">
+                Ver más
+            </button>
+        </div>
+    </div>
+    <div class="bg-blue-200 rounded-lg shadow-2x1 w-1/5 m-5">
         <img src="public/images/php_logo.png" alt="" 
                 class="rounded-t-lg h-48 w-full">
         <h2 class="text-x1 font-extrabold px-8">PHP</h2>
@@ -13,6 +25,30 @@
             <p class="truncate">PHP (acrónimo recursivo de PHP: Hypertext Preprocessor) es un 
                 lenguaje de código abierto muy popular especialmente adecuado 
                 para el desarrollo web y que puede ser incrustado en HTML.</p>
+            <button class="bg-blue-500 text-blue-100 rounded-lg py-2 px-4 mt-5">
+                Ver más
+            </button>
+        </div>
+    </div>
+    <div class="bg-blue-200 rounded-lg shadow-2x1 w-1/5 m-5">
+        <img src="public/images/css3_logo.png" alt="" 
+                class="rounded-t-lg h-48 w-full">
+        <h2 class="text-x1 font-extrabold px-8">CSS 3</h2>
+        <div class="pl-8 pr-8 pb-8">
+            <p class="truncate">CSS (en inglés Cascading Style Sheets) es lo que se 
+                denomina lenguaje de hojas de estilo en cascada y se usa para estilizar</p>
+            <button class="bg-blue-500 text-blue-100 rounded-lg py-2 px-4 mt-5">
+                Ver más
+            </button>
+        </div>
+    </div>
+    <div class="bg-blue-200 rounded-lg shadow-2x1 w-1/5 m-5">
+        <img src="public/images/tailwindcss_logo.png" alt="" 
+                class="rounded-t-lg h-48 w-full">
+        <h2 class="text-x1 font-extrabold px-8">Tailwind CSS</h2>
+        <div class="pl-8 pr-8 pb-8">
+            <p class="truncate">Tailwind CSS es un framework CSS que permite un desarrollo ágil, 
+                basado en clases de utilidad que se pueden aplicar con facilidad en el código</p>
             <button class="bg-blue-500 text-blue-100 rounded-lg py-2 px-4 mt-5">
                 Ver más
             </button>
